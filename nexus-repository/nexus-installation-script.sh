497d8ca7a2f6333f5979c13a9078a9aa1484aa79
Kise2093 <people12337@gmail.com>
2022-02-10 11:43:39 +0700

    ftg

--- nexus-repository/nexus-installation-script.sh
+++ nexus-repository/nexus-installation-script.sh
@@ -2,5 +2,5 @@
 
 helm repo add sonatype https://sonatype.github.io/helm3-charts/
 helm repo update
-helm install nexus-rm sonatype/nexus-repository-manager [ --version v29.2.0 ]
+helm install -n nexus-rm nexus-rm sonatype/nexus-repository-manager [ --version v29.2.0 ]
 
