a52686f8c9011edcc5d0149a29bde5593dd5f5b8
Kise2093 <people12337@gmail.com>
2022-03-03 20:00:52 +0700

    new

--- gitlab/gitlab-installation-script.sh
+++ gitlab/gitlab-installation-script.sh
@@ -30,8 +30,10 @@ helm upgrade --install gitlab gitlab/gitlab \
 helm upgrade --install -n gitlab gitlab gitlab/gitlab \
   --set global.edition=ce \
   --set global.hosts.domain=bci.ikp.com \
-  --set certmanager-issuer.email=kris@iknowplus.co.th
-  --set global.ingress.configureCertmanager=false
+  --set certmanager-issuer.email=kris@gmail.com \
+  --set nginx-ingress.enabled=false \
+  --set global.ingress.configureCertmanager=false \
+  --set global.ingress.annotations."cert-manager\.io/cluster-issuer"=letsencrypt
 
 helm -n gitlab install gitlab gitlab/gitlab \
   --set certmanager.install=false \
@@ -39,29 +41,22 @@ helm -n gitlab install gitlab gitlab/gitlab \
   --set certmanager-issuer.email=kris@iknowplus.co.th \
   --set global.hosts.domain=bci.ikp.com \
   --set global.ingress.annotations."kubernetes\.io/tls-acme"=true \
-  --set global.ingress.annotations."cert-manager\.io/cluster-issuer"=letsencrypt-production \
-  --set gitlab.webservice.ingress.tls.secretName=gitlab-gitlab-tls \
-  --set registry.ingress.tls.secretName=gitlab-registry-tls \
-  --set minio.ingress.tls.secretName=gitlab-minio-tls \
-  --set gitlab-runner.install=false
-
-#current
-
-helm -n gitlab install gitlab gitlab/gitlab \
-  --set certmanager.install=false \
-  --set global.ingress.configureCertmanager=false \
-  --set certmanager-issuer.email=kris@iknowplus.co.th \
-  --set global.hosts.domain=bci.ikp.com \
   --set global.ingress.annotations."cert-manager\.io/cluster-issuer"=letsencrypt \
   --set gitlab.webservice.ingress.tls.secretName=gitlab-gitlab-tls \
   --set registry.ingress.tls.secretName=gitlab-registry-tls \
   --set minio.ingress.tls.secretName=gitlab-minio-tls \
-  --set gitlab-runner.install=false
+  --set gitlab-runner.install=false \
+  --set nginx-ingress.enabled=false
+
+helm upgrade --install --namespace gitlab gitlab-runner gitlab/gitlab-runner \
+  --set gitlabUrl=https://gitlab.bci.iknowplus.co.th,runnerRegistrationToken=fpaqXfVQzcKQ6zWBsmA6
+
+
 
 helm upgrade --install \
   cert-manager jetstack/cert-manager \
   --namespace cert-manager \
   --create-namespace \
-  --version v1.7.1 \
   --set installCRDs=true \
+  --version v1.7.1 \
   --set 'extraArgs={--acme-http01-solver-nameservers=10.1.0.13:53\,10.96.0.10:53\,192.168.1.1:53}'
